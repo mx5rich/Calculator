7731aca19bb8b3edaaaea80e6971b3a5ef464e95
Ricardo Barajas <rb1.mx5@gmail.com>
2023-01-22 23:27:42 -0700

    created function to appen number selected and display on calculator screen

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ class Calculator {
     constructor(previousOperandTextElement, currentOperandTextElement) {
         this.previousOperandTextElement = previousOperandTextElement
         this.currentOperandTextElement = currentOperandTextElement
+        this.clear()
     }
 
     clear() {
-
+        this.currentOperand = ''
+        this.previousOperand = ''
+        this.operation = undefined
     }
 
     delete() {
@@ -13,8 +16,10 @@ class Calculator {
     }
 
     appendNumber(number) {
+    if (number === '.' && this.currentOperand.includes('.')) return
+    this.currentOperand = this.currentOperand + number.toString()
+  }
 
-    }
 
     chooseOperation(operation) {
 
@@ -24,8 +29,8 @@ class Calculator {
 
     }
 
-    updateDisplay(){
-
+    updateDisplay() {
+        this.currentOperandTextElement.innerText = this.currentOperand
     }
 }
 
@@ -33,7 +38,15 @@ const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
 const equalsButton = document.querySelector('[data-equals]')
 const deleteButton = document.querySelector('[data-delete]')
-const allClearButton = docmuent.querySelector('[data-all-clear]')
+const allClearButton = document.querySelector('[data-all-clear]')
 const previousOperandTextElement = document.querySelector('[data-previous-operand')
 const currentOperandTextElement = document.querySelector('[data-current-operand')
 
+const calculator = new Calculator (previousOperandTextElement, currentOperandTextElement)
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
